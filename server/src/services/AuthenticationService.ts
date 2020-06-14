9bdb4cbe0dcf9838fb394eb9db6523758780abd2
Martin PELCAT <martinpelcat@hotmail.com>
2020-06-14 10:42:59 +0200

    refactor clean code

--- server/src/services/AuthenticationService.ts
+++ server/src/services/AuthenticationService.ts
@@ -1,41 +1,46 @@
 import { IUser } from "../models";
-import { UserDAO } from '../dao';
+import { UserDAO } from "../dao";
 import { FilterQuery } from "mongoose";
 import { comparePassword } from "../utils";
 
 class AuthenticationService {
-    private userDAO = UserDAO;
-    public getCurrentUser(): Promise<IUser | null> {
-        return new Promise((resolve, reject) => {
-            //TODO return current user if connected
-        });
-    }
+  private userDAO = UserDAO;
+  public getCurrentUser(): Promise<IUser | null> {
+    return new Promise((resolve, reject) => {
+      //TODO return current user if connected
+    });
+  }
 
+  public generateNewToken(user: IUser): Promise<IUser> {
+    return this.userDAO.generateNewToken(user);
+  }
 
-    public generateNewToken(user: IUser): Promise<IUser> {
-        return this.userDAO.generateNewToken(user);
-    }
-
-    public findUserOrFail(condition: FilterQuery<IUser>, password: string): Promise<IUser> {
-        return new Promise<IUser>((resolve, reject) => {
-            this.userDAO.findOne(condition).then((user) => {
-                if (user) {
-                    comparePassword(password, user.password).then(() => {
-                        user.password = undefined; // dont send back password in response
-                        resolve(user);
-                    }).catch(() => {
-                        reject(new Error("User not found"));
-                    })
-                } else {
-                    reject(new Error("User not found"));
-                }
-            }).catch((err) => {
-                reject(err);
-            })
+  public findUserOrFail(
+    condition: FilterQuery<IUser>,
+    password: string
+  ): Promise<IUser> {
+    return new Promise<IUser>((resolve, reject) => {
+      this.userDAO
+        .findOne(condition)
+        .then((user) => {
+          if (user && user.password) {
+            comparePassword(password, user.password)
+              .then(() => {
+                user.password = undefined; // dont send back password in response
+                resolve(user);
+              })
+              .catch(() => {
+                reject(new Error("User not found"));
+              });
+          } else {
+            reject(new Error("User not found"));
+          }
         })
-    }
-
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
 }
-
 
 export default new AuthenticationService();
